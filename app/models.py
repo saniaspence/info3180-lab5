947514d00f897fb0578ffc53f1e93ed073f60ece
Sania Spence <saniaspence90210@gmail.com>
2020-03-01 20:19:08 -0500

    created new migration

--- app/models.py
+++ app/models.py
@@ -12,6 +12,13 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
+
+    def _init_(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
 
     def is_authenticated(self):
         return True
